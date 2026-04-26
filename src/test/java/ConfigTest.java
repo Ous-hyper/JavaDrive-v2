5875440cd3c87000791c73ceb3dbfd61117579c2
Usama <13215484@alu.murciaeduca.es>
2026-04-26 19:36:11 +0200

    add Test's

--- src/test/java/ConfigTest.java
+++ src/test/java/ConfigTest.java
@@ -1,2 +1,38 @@
-public class ConfigTest {
-}
+
+import logica.GestorClientes;
+import logica.GestorFlota;
+import model.Cliente;
+import model.Coche;
+import model.TipoCoche;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+    public class ConfigTest {
+
+        @Test
+        public void testCrearCliente() {
+            Cliente c = new Cliente("12345678A", "Juan", "600000000");
+            assertNotNull(c);
+            assertEquals("Juan", c.getNombre());
+        }
+
+        @Test
+        public void testCrearCoche() {
+            Coche coche = new Coche("1234ABC", "Toyota", "Corolla", true, TipoCoche.Deportivo, 5);
+            assertNotNull(coche);
+            assertEquals("1234ABC", coche.getMatricula());
+        }
+
+        @Test
+        public void testGestorClientes() {
+            GestorClientes gc = new GestorClientes();
+            assertNotNull(gc);
+        }
+
+        @Test
+        public void testGestorFlota() {
+            GestorFlota gf = new GestorFlota();
+            assertNotNull(gf);
+        }
+    }
+
